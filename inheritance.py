51e47f738982d0d3d3cba9ededd276a55177b5e5
sarvesh joshi <sarveshggn@gmail.com>
2019-01-13 22:28:39 +0530

    inheritance example file added

---
+++ inheritance.py
@@ -0,0 +1,27 @@
+class Animal():
+
+    def __init__(self):
+        print("ANIMAL CREATED")
+
+    def who_am_i(self):
+        print("ANIMAL")
+
+    def eat(self):
+        print("EATING")
+
+class Dog(Animal):
+
+    def __init__(self):
+        print("DOG CREATED")
+
+    def bark(self):
+        print("WOOF WOOF!!!")
+
+    def eat(self):
+        print("DOG EATING")              ####     OVERWRITING THE BASE CLASS    ####
+
+
+mydog = Dog()
+mydog.who_am_i()
+mydog.eat()
+mydog.bark()
